747cc9bbde84fc6308b796ef8c2e79f7c610cc97
Rmckays <rmckays@gmail.com>
2023-02-20 18:44:18 -0700

    Add initial findClosestValueInBst.

---
+++ findClosestValueInBst/findClosestValueInBst.js
@@ -0,0 +1,14 @@
+function findClosestValueInBst(tree, target) {
+  // Write your code here.
+}
+
+// This is the class of the input tree. Do not edit.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+module.exports = findClosestValueInBst;
